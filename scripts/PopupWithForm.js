3f0d3d6c077e13e7e1ef9f4f4994707e6bb8731e
IxxyDev <denisenkoforwork@gmail.com>
2020-06-18 00:16:55 +0300

    refactor: add PopupWithForm class

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,47 @@
+import Popup from './Popup.js';
+import { popupConfig, validationConfig } from '../utils/utils.js';
+
+//Реализацию с fillInputs (для двух разных форм) подсказали в slack((
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, { handleFormSubmit, fillInputs }) {
+        super(popupSelector);
+        this._fillInputs = fillInputs;
+        this._handleFormSubmit = handleFormSubmit;
+    }
+
+    _clearFormErrors() {
+        this._inputList = this._popupSelector.querySelectorAll(validationConfig.inputSelector);
+        this._inputErrorList = this._popupSelector.querySelectorAll(validationConfig.errorSelector);
+        inputList.forEach((input) => {
+            input.classList.remove(validationConfig.errorClass);
+        });
+        inputErrorList.forEach((errorElement) => {
+          errorElement.textContent = '';
+        });
+      }
+
+    _resetButtonState() {
+        this._submitButton = this._popupSelector.querySelector(popupConfig.submitButtonSelector);
+        this._submitButton.disabled = true;
+        this._submitButton.classList.add(validationConfig.inactiveButtonClass);
+    }
+
+    _getInputValues() {
+        this._inputValues = {};
+        this._inputList.forEach((input) => {
+            this._inputValues[input.name] = input.value;
+            return this._inputValues;
+        })
+    }
+
+    open() {
+        this._resetButtonState();
+        this._fillInputs();
+        super.open();
+    }
+
+    close() {
+        this.popupSelector.querySelector(popupConfig.formSelector).reset();
+        super.close();
+    }
+}
--- utils/utils.js
+++ utils/utils.js
@@ -25,6 +25,7 @@ export const cardConfig = {
 export const validationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
+    errorSelector: '.popup__input-error_active',
     submitButtonSelector: '.popup__button',
     inactiveButtonClass: 'popup__button_disabled',
     inputErrorClass: 'popup__input_type_error',
